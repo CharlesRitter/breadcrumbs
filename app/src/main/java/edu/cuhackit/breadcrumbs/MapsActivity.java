51a958594e1d0f1425e60f39a4e40cd1eede75ee
trannikita <nikiusa09@yahoo.com>
2020-01-25 17:57:09 -0500

    Update

--- app/src/main/java/edu/cuhackit/breadcrumbs/MapsActivity.java
+++ app/src/main/java/edu/cuhackit/breadcrumbs/MapsActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -26,6 +27,7 @@ import org.jetbrains.annotations.Nullable;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    final static String TAG = "MapsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        Log.i(TAG, "Tracking: " + Radar.isTracking());
     }
 
 
@@ -56,32 +59,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
 
-        Radar.initialize("prj_test_pk_b2451fcc967db99f2912c986d8c75cea785ca5d0");
-        MyRadarReceiver mine = new MyRadarReceiver();
+        //Radar.initialize("prj_test_pk_b2451fcc967db99f2912c986d8c75cea785ca5d0"); //Jemiah's key
+        Radar.initialize("prj_test_pk_668bab55b5fbac2e7a4a28247c3d57ccfb5160e3"); //Nikita's key
 
         Radar.trackOnce(new Radar.RadarCallback() {
             @Override
             public void onComplete(Radar.RadarStatus status, Location location, RadarEvent[] events, RadarUser user) {
 
-                LatLng first = new LatLng(location.getLatitude(), location.getLongitude());
+                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
-                mMap.addMarker(new MarkerOptions().position(first).title("You are Here"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(first));
-
-                /*LatLng walk = new LatLng(first.latitude + .5, first.longitude + .5);
-                location.setLatitude(walk.latitude);
-                location.setLongitude(walk.longitude);
-
-                Radar.updateLocation(location, new Radar.RadarCallback(){
-                    @Override
-                    public void onComplete(@NotNull Radar.RadarStatus radarStatus, @Nullable Location location, @Nullable RadarEvent[] radarEvents, @Nullable RadarUser radarUser) {
-
-                    }
-                });*/
+                mMap.addMarker(new MarkerOptions().position(currentLocation).title("You are Here"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,
+                        20));
 
             }
         });
 
         Radar.startTracking();
+    }
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Radar.stopTracking();
     }
 }
--- app/src/main/java/edu/cuhackit/breadcrumbs/MyRadarReceiver.java
+++ app/src/main/java/edu/cuhackit/breadcrumbs/MyRadarReceiver.java
@@ -7,27 +7,33 @@ import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.text.DecimalFormat;
+
 import io.radar.sdk.Radar;
 import io.radar.sdk.RadarReceiver;
 import io.radar.sdk.model.RadarEvent;
+import io.radar.sdk.model.RadarGeofence;
 import io.radar.sdk.model.RadarUser;
 
 public class MyRadarReceiver extends RadarReceiver {
+    final static String TAG = "RadarReceiver";
 
     @Override
     public void onError(@NotNull Context context, @NotNull Radar.RadarStatus radarStatus) {
-
+        Log.i(TAG, "onError");
     }
 
     @Override
     public void onLocationUpdated(@NotNull Context context, Location location, RadarUser user) {
+        Log.i(TAG, "onLocationUpdated: " + location.getLatitude() + " " + location.getLongitude());
+        Toast.makeText(context, "Location updated", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void onEventsReceived(@NotNull Context context, @NotNull RadarEvent[] radarEvents, @NotNull RadarUser radarUser) {
-        Toast.makeText(context, "I got something", Toast.LENGTH_SHORT).show();
-        Log.i("Something", radarEvents[0].toString());
-
+        Log.i(TAG, "onEventsReceived: " +
+                "Length: " + radarEvents.length);
+        Toast.makeText(context, "Event Triggered", Toast.LENGTH_SHORT).show();
     }
 }
