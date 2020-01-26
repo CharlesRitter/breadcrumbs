de9936659bd433e01bfe612adc1bbe4b50f63b36
Charles Ritter <crritte@g.clemson.edu>
2020-01-26 08:04:45 -0500

    please work

---
+++ app/src/main/java/edu/cuhackit/breadcrumbs/Form.java
@@ -0,0 +1,70 @@
+package edu.cuhackit.breadcrumbs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Form extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
+    private static final int GALLERY_REQUEST_CODE = 123;
+    ImageView imageView;
+    Button photoGallery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        mTopToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+
+    }
+
+    public void share_post(View v){
+        EditText caption_textV = findViewById(R.id.caption_text);
+        String cap_text = caption_textV.getText().toString();
+
+        if (cap_text.length() == 0 ){
+            Toast.makeText(getApplicationContext(),
+                    "Please write a caption", Toast.LENGTH_LONG).show();
+        } else
+            {
+                //Add caption to database
+            }
+    }
+
+    public void pull_gallery(View v){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        startActivityForResult(intent,GALLERY_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData returns the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    imageView.setImageURI(selectedImage);
+                    break;
+            }
+    }
+
+}
--- app/src/main/java/edu/cuhackit/breadcrumbs/MapsActivity.java
+++ app/src/main/java/edu/cuhackit/breadcrumbs/MapsActivity.java
@@ -85,6 +85,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 
         Radar.startTracking();
+
     }
 
 
